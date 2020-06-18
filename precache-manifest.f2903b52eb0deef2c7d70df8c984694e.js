609167aafcbdd734f4311cd8fe58203d9123d5ef
supriya <linkin.suppu@gmail.com>
2020-06-18 16:15:31 +0545

    Updates

---
+++ precache-manifest.f2903b52eb0deef2c7d70df8c984694e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243b2d99ec233a35cf0089d79d993013",
+    "url": "/leapfrog-intern-projects/index.html"
+  },
+  {
+    "revision": "4733b0c175b777d65bdd",
+    "url": "/leapfrog-intern-projects/static/css/main.c1573472.chunk.css"
+  },
+  {
+    "revision": "8474ae4b75675eaac8b6",
+    "url": "/leapfrog-intern-projects/static/js/2.da7a1738.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/leapfrog-intern-projects/static/js/2.da7a1738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4733b0c175b777d65bdd",
+    "url": "/leapfrog-intern-projects/static/js/main.3626b95e.chunk.js"
+  },
+  {
+    "revision": "87d09e654750b15da292",
+    "url": "/leapfrog-intern-projects/static/js/runtime-main.5c5a2ec9.js"
+  },
+  {
+    "revision": "f3f96bd0b9fca4febe171d9259166364",
+    "url": "/leapfrog-intern-projects/static/media/ant-smasher.f3f96bd0.png"
+  },
+  {
+    "revision": "672d60d7545e136e23a78d43ebd64d55",
+    "url": "/leapfrog-intern-projects/static/media/background.672d60d7.png"
+  },
+  {
+    "revision": "599aad7a062db524ce7252ee23e38c57",
+    "url": "/leapfrog-intern-projects/static/media/bumpsheep.599aad7a.png"
+  },
+  {
+    "revision": "43a0c9fc5f20bc8d95250b1ba44c7a39",
+    "url": "/leapfrog-intern-projects/static/media/car-game.43a0c9fc.png"
+  },
+  {
+    "revision": "18c4c4a5d38d07a0608298a32232d7e4",
+    "url": "/leapfrog-intern-projects/static/media/design-2.18c4c4a5.png"
+  },
+  {
+    "revision": "caf14302cc159022de72a2a132941c80",
+    "url": "/leapfrog-intern-projects/static/media/design-final-2.caf14302.png"
+  },
+  {
+    "revision": "fa810b4e8bd95f778d77c3af39be3844",
+    "url": "/leapfrog-intern-projects/static/media/design-final.fa810b4e.png"
+  },
+  {
+    "revision": "3392e01a1fcd060474b500f9f38341d5",
+    "url": "/leapfrog-intern-projects/static/media/flappy-bird.3392e01a.png"
+  },
+  {
+    "revision": "36300d6319d7ebf125ad138073a291f2",
+    "url": "/leapfrog-intern-projects/static/media/helix.36300d63.png"
+  },
+  {
+    "revision": "e689e3cfeac21288aefef766c39f49ca",
+    "url": "/leapfrog-intern-projects/static/media/image-slider.e689e3cf.png"
+  },
+  {
+    "revision": "3424b4c0f59301b406aa8688cc5cda80",
+    "url": "/leapfrog-intern-projects/static/media/profile.3424b4c0.jpg"
+  },
+  {
+    "revision": "2704b3fa69a484be53e022061bbe332e",
+    "url": "/leapfrog-intern-projects/static/media/tollens.2704b3fa.png"
+  }
+]);
